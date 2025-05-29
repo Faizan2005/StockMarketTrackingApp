1b34480605e431642d98e0030e1b41333f0a92c0
Faizan2005 <illusionbuster05@gmail.com>
2025-05-29 23:29:41 +0530

    fix

--- models/schema.js
+++ models/schema.js
@@ -1,15 +1,1 @@
-const mongoose = require("mongoose");
 
-const quoteSchema = new mongoose.Schema({
-  symbol: String,
-  price: Number,
-  open: Number,
-  high: Number,
-  low: Number,
-  close_previous: Number,
-  change: Number,
-  change_percent: Number,
-  timeStamp: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Quote", quoteSchema);
